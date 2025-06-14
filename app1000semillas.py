4137c278af8600cd352f57e6b3c781b4aff3c233
BdG-UNLP <bancodegermoplasmaunlp@gmail.com>
2025-06-14 20:49:54 -0300

    Update app1000semillas.py

--- app1000semillas.py
+++ app1000semillas.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Análisis de Pesadas", layout="centered")
 
 st.title("🌾 Análisis de Peso de 1000 Semillas")
 st.write(
-    "Cálculo del peso promedio de 1000 semillas a partir del peso de ocho o más réplicas de 100 semillas..."
+    "Cálculo del peso promedio de 1000 semillas a partir del peso de ocho o más réplicas de 100 semillas."
 )
 
 # Elegir método de entrada
