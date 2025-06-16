36b9992378086c36f19961787fe46825b7e3be90
BdG-UNLP <bancodegermoplasmaunlp@gmail.com>
2025-06-16 10:08:09 -0300

    home.py

--- app/home.py
+++ app/home.py
@@ -1,1 +1,16 @@
+# -----------------------------
+# 📄 Home.py
+# -----------------------------
+import streamlit as st
 
+st.set_page_config(page_title="Inicio", layout="centered")
+
+st.title("🌾 Bienvenido a la Aplicación Agronómica")
+st.markdown("""
+Esta aplicación incluye varias herramientas:
+
+- 📊 **Análisis de peso de 1000 semillas**
+- 🌱 **Diseño Experimental a Campo**
+
+Usá el menú de la izquierda para navegar entre módulos.
+""")
